fae6de4389e8d51f4fc4a2f164da355e5386ccba
INAcodes <93144864+INAcodes@users.noreply.github.com>
2022-02-18 16:05:38 +0100

    added weather component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by <em>INA HADAJ</em> and is {" "}
-        <a href="" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
-      
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by <em>INA HADAJ</em> and is{" "}
+          <a
+            href="https://github.com/INAcodes/new-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
