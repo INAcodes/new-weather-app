0e3592bcf35711ce093d0b50d7ff55e5699c75a2
INAcodes <93144864+INAcodes@users.noreply.github.com>
2022-02-18 19:13:14 +0100

    added footer css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from "react";
-import './App.css';
 import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
+
         <footer>
           This project was coded by <em>INA HADAJ</em> and is{" "}
           <a
             href="https://github.com/INAcodes/new-weather-app"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
             open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://focused-lalande-d12501.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
           </a>
         </footer>
       </div>
