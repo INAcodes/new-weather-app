fec66c433790558265d369eed65bbd98df295dc5
INAcodes <93144864+INAcodes@users.noreply.github.com>
2022-02-20 12:00:32 +0100

    update weathericon

--- src/Weather.js
+++ src/Weather.js
@@ -57,7 +57,7 @@ function search(){
               <input
                 type="submit"
                 value="Search"
-                className="btn btn-primary w-100"
+                className="btn btn-light w-100"
               />
             </div>
           </div>
